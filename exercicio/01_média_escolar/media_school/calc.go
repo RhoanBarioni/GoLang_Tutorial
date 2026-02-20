74873f010fc8184cfac3e4109e5bdcfa3bce77f6
RhoanBarioni <rhoanbarioni@gmail.com>
2026-02-20 20:00:09 -0300

    more better

--- exercicio/01_média_escolar/media_school/calc.go
+++ exercicio/01_média_escolar/media_school/calc.go
@@ -3,11 +3,13 @@ package mediaschool
 import (
 	"fmt"
 )
-func Calc(nota1, nota2, nota3, nota4, nota5 float64)(float64){
-	mediaNota := (nota1 + nota2 + nota3 + nota4 + nota5)/5
+func Calc(notas float64)(float64){
+	mediaNota := notas/5
 
 	if mediaNota < 7 {
 		fmt.Println("Reprovado, seu burro")
-	} else {fmt.Println("Aprovado, meu nego bom")}
+	} else {
+		fmt.Println("Aprovado, meu nego bom")
+	}
 	return mediaNota
 }
--- exercicio/01_média_escolar/nota.go
+++ exercicio/01_média_escolar/nota.go
@@ -6,5 +6,19 @@ import (
 )
 
 func main(){
-	fmt.Println(mediaschool.Calc(10, 10, 10, 5, 10))
+	var nota1, nota2, nota3, nota4, nota5 float64
+	fmt.Println("Primeira Nota: ")
+	fmt.Scanln(&nota1)
+	fmt.Println("Segunda Nota: ")
+	fmt.Scanln(&nota2)
+	fmt.Println("Terceira Nota: ")
+	fmt.Scanln(&nota3)
+	fmt.Println("Quarta Nota: ")
+	fmt.Scanln(&nota4)
+	fmt.Println("Quinta Nota: ")
+	fmt.Scanln(&nota5)
+
+	notas := nota1 + nota2 + nota3 + nota4 + nota5
+
+	fmt.Println(mediaschool.Calc(notas))
 }
